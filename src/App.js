a0331669bd418a7c33fbcafb0d7dd6485a90c120
Robin Wassbjer <robin.wassbjer@gmail.com>
2019-11-18 19:12:25 +0100

    Add user search function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment, Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -12,11 +13,22 @@ class App extends Component {
 
   async componentDidMount() {
     this.setState({ loading: true });
-
-    const res = await axios.get('https://api.github.com/users');
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GH_CLIENT_ID}&client_secret=${process.env.REACT_APP_GH_CLIENT_SECRET}`
+    );
 
     this.setState({ users: res.data, loading: false });
   }
+
+  searchUsers = async text => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GH_CLIENT_ID}&client_secret=${process.env.REACT_APP_GH_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     const { loading, users } = this.state;
@@ -25,6 +37,7 @@ class App extends Component {
       <Fragment>
         <Navbar></Navbar>
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={loading} users={users}></Users>
         </div>
       </Fragment>
