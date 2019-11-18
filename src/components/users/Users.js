2d768debf2701860465bfe713210c88fad49224c
Robin Wassbjer <robin.wassbjer@gmail.com>
2019-11-18 18:06:22 +0100

    Install axios and fetch users from Github API

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,33 +2,10 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 55049918,
-        login: 'Wassberg',
-        avatar_url: 'https://avatars2.githubusercontent.com/u/55049918?v=4',
-        html_url: 'https://github.com/Wassberg'
-      },
-      {
-        id: 55049919,
-        login: 'Wassberg',
-        avatar_url: 'https://avatars2.githubusercontent.com/u/55049918?v=4',
-        html_url: 'https://github.com/Wassberg'
-      },
-      {
-        id: 55049920,
-        login: 'Wassberg',
-        avatar_url: 'https://avatars2.githubusercontent.com/u/55049918?v=4',
-        html_url: 'https://github.com/Wassberg'
-      }
-    ]
-  };
-
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user}></UserItem>
         ))}
       </div>
