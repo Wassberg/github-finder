4a28a0d7bb37c7d3eaa7321d57b389ab6795e8ce
Robin Wassbjer <robin.wassbjer@gmail.com>
2019-11-27 12:52:59 +0100

    Add about page and simple routing with 'react-router-dom'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Fragment, Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
@@ -45,19 +47,32 @@ class App extends Component {
     const { loading, users } = this.state;
 
     return (
-      <Fragment>
-        <Navbar></Navbar>
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users}></Users>
-        </div>
-      </Fragment>
+      <Router>
+        <Fragment>
+          <Navbar></Navbar>
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users}></Users>
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" Component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
     );
   }
 }
